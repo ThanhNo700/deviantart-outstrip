0e4c37767c4607233de1f00e7bd00f3bcc695ba6
Quintino <94907652+ThanhNo700@users.noreply.github.com>
2024-07-11 08:16:01 +0700

    Included a Javascript

---
+++ changelog.js
@@ -0,0 +1,42 @@
+function handleChangelog() {
+	let changelogHeader = document.querySelector('#youtube-redux-header');
+	let changelogVersion = document.querySelector('#version');
+	let version = chrome.runtime.getManifest().version;
+	if (!changelogHeader) return;
+	if (window.location.href.includes('#install')) {
+		changelogHeader.innerText = 'YouTube Redux has been installed!';
+	} else {
+		changelogVersion.innerText = ` to v.${version}`;
+	}
+}
+
+function addShowMore() {
+	const updatesToDisplay = 3;
+	let counter = 0;
+	let changesContainer = document.querySelector('#changes ul');
+	let showMoreButton = document.createElement('div');
+	showMoreButton.classList.add('show-more');
+	showMoreButton.innerText = 'Show more...';
+	showMoreButton.addEventListener('click', function () {
+		for (let i = 0; i < changesContainer.children.length; i++) {
+			changesContainer.children[i].removeAttribute('hidden');
+		}
+		this.remove();
+	});
+	
+
+	for (let i = 0; i < changesContainer.children.length; i++) {
+		const currentElement = changesContainer.children[i];
+		const showMoreElement = document.querySelector('.show-more');
+		if (currentElement.classList && currentElement.classList.contains('update')) counter++;
+		if (!showMoreElement && counter > updatesToDisplay) {
+			changesContainer.insertBefore(showMoreButton, currentElement);
+		}
+		if (showMoreElement) {
+			currentElement.setAttribute('hidden', '');
+		}
+	}
+}
+
+handleChangelog();
+addShowMore();
